50c992ae66f87c1ebcfbb6af5f89b994d3f26398
Tanmay Kejriwal <52064142+tanmay-kali@users.noreply.github.com>
2023-02-14 12:08:44 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+      <>
+        <h1>React Starter Template</h1>
+        <p>Comes with styled components and babel presets</p>
+      </>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from "./App";
+import "./App.css";
+
+const el = document.getElementById("app");
+const root = createRoot(el);
+root.render(<App />);
